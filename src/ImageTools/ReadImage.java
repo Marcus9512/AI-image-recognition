0a7264a256985a0832b933b66640df8505f97aa9
Marcus Jonsson Ewerbring <marcusew@kth.se>
2018-12-18 12:07:26 +0100

    created a class for reading image

--- src/ImageTools/ReadImage.java
+++ src/ImageTools/ReadImage.java
@@ -1,21 +1,39 @@
 package ImageTools;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class ReadImage {
 
-    private String path;
     private File[] imagePathList;
-
+    private int currentImage = 0;
 
     public ReadImage(String path){
-        this.path = path;
+        createImageList(path);
     }
     private void createImageList(String path){
         File folder = new File(path);
         imagePathList = folder.listFiles();
+    }
+    private BufferedImage getNextImage(){
+        File tmp;
+        if(currentImage < imagePathList.length) {
+            tmp = imagePathList[currentImage];
+            currentImage++;
+        }else{
+            return null;
+        }
+        try {
+            return ImageIO.read(tmp);
+
+        } catch (IOException e) {
+            System.err.println("File not found");
+        }
+        return null;
     }
 
 }
---
+++ src/ImageTools/TestImageClass.java
@@ -0,0 +1,9 @@
+package ImageTools;
+
+public class TestImageClass {
+    public static void main(String[] args) {
+        ReadImage test = new ReadImage("/bilder");
+        
+
+    }
+}
