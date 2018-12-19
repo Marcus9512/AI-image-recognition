585ee10918a2644cd3cdc0c186c7cd0f9e33b237
Marcus Jonsson Ewerbring <marcusew@kth.se>
2018-12-19 16:08:43 +0100

    started to implement backprop

--- src/CNN/Cnn.java
+++ src/CNN/Cnn.java
@@ -5,6 +5,7 @@ import ActivationFunctions.ReLu;
 import ImageTools.ReadImage;
 import Tools.MyMath;
 
+import javax.tools.Tool;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
@@ -25,6 +26,10 @@ public class Cnn {
     ArrayList<Double[][]> weights = new ArrayList<>();
     ReadImage trainMaterial1;
 
+    ArrayList<Double[][]> gradientWeights = new ArrayList<>();
+    ArrayList<double[]> gradientBias = new ArrayList<>();
+    ArrayList<double[]> gradientCost = new ArrayList<>();
+
     public Cnn(){
 
         init();
@@ -38,7 +43,7 @@ public class Cnn {
         while ((bi = trainMaterial1.getNextImage()) != null || epochs < maxEpochs){
             decodeImageToInputLayer(bi);
             forwardProp();
-            backwardProp();
+            backwardProp(0);
         }
     }
 
@@ -61,10 +66,46 @@ public class Cnn {
             }
         }
     }
-    private void backwardProp(int solutionIndex){
+    private void backwardProp(double solutionIndex){
         //för varje nod utom input:
         //cost = (faktiskt värde - önskat värde)^2
 
+        Perceptron[] lastLayer = layers[layers.length-1].getPerceptrons();
+        double[] cost = gradientCost.get(layers.length-1);
+
+        for(int i = 0; i<cost.length;i++){
+            if(i == solutionIndex){
+                cost[i] = (lastLayer[i].getOutput() - 1) * (lastLayer[i].getOutput() - 1);
+            }else{
+                cost[i] = (lastLayer[i].getOutput()) * (lastLayer[i].getOutput());
+            }
+        }
+
+        for(int i = layers.length-2; i>=0 ; i--){
+            Perceptron[] perceptronsInCL = layers[i].getPerceptrons();
+            Perceptron[] perceptronsInPL = layers[i+1].getPerceptrons();
+            Double[][] currentWeights = weights.get(i);
+
+            for(int j = 0; j< perceptronsInCL.length; i++){
+                double z = 0;
+                gradientCost.get(i)[j] = 0;
+                for(int w = 0 ; w < currentWeights.length; w++){
+
+                    z+= currentWeights[w][j] *  perceptronsInPL[w].getOutput();
+                    z+= perceptronsInCL[j].getBias();
+
+                    double correctionWeight = 0;
+
+                    correctionWeight = MyMath.Dcdw(perceptronsInCL[j].getOutput(),gradientCost.get(i+1)[w] , z, perceptronsInCL[j].getActivationFunction(), perceptronsInPL[w]);
+                    weights.get(i)[w][j] += correctionWeight;
+
+                    gradientCost.get(i)[j] += MyMath.Dcda(perceptronsInCL[j].getOutput(),gradientCost.get(i+1)[w] , z, perceptronsInCL[j].getActivationFunction(), currentWeights[w][j]);
+
+                }
+
+
+            }
+        }
 
 
     }
@@ -81,20 +122,39 @@ public class Cnn {
         layers[3] = new Layer(3,hiddenSize, relu);
         layers[4] = new Layer(4,numberOfOutputs, relu);
 
+        gradientBias.add(new double[picH*picW]);
+        gradientBias.add(new double[hiddenSize]);
+        gradientBias.add(new double[hiddenSize]);
+        gradientBias.add(new double[hiddenSize]);
+        gradientBias.add(new double[numberOfOutputs]);
+
+        gradientCost.add(new double[picH*picW]);
+        gradientCost.add(new double[hiddenSize]);
+        gradientCost.add(new double[hiddenSize]);
+        gradientCost.add(new double[hiddenSize]);
+        gradientCost.add(new double[numberOfOutputs]);
+
         for(int i = 0 ; i< layers.length-1;i++){
             //create weight matrix between each layer
-            createWeightMatrix(layers[i].getPerceptrons().length,layers[i+1].getPerceptrons().length);
+            createWeightMatrix(layers[i].getPerceptrons().length,layers[i+1].getPerceptrons().length,weights,false);
+            createWeightMatrix(layers[i].getPerceptrons().length,layers[i+1].getPerceptrons().length,gradientWeights,true);
         }
+
+
     }
-    private void createWeightMatrix(int x,int y){
+    private void createWeightMatrix(int x,int y,ArrayList<Double[][]> addto ,boolean onlyZeros){
         Double[][] tmp = new Double[x][y];
         for(int i = 0; i< x ; i++){
             for(int j = 0; j < y ; j++){
-                tmp[i][j] = new Double(MyMath.rand(-10,10));
+                if(!onlyZeros)
+                    tmp[i][j] = new Double(MyMath.rand(-10,10));
+                else
+                    tmp[i][j] = 0.0;
             }
         }
-        weights.add(tmp);
+        addto.add(tmp);
     }
+
     private void saveNetwork(String name){
         Tools.SaveAndLoadNetwork.save(layers,weights,name);
     }
--- src/Tools/MyMath.java
+++ src/Tools/MyMath.java
@@ -2,6 +2,7 @@ package Tools;
 
 import CNN.Layer;
 import CNN.Perceptron;
+import ActivationFunctions.ActivationFunction;
 
 import java.util.Random;
 
@@ -13,22 +14,25 @@ public class MyMath {
         return random.nextInt(high-low)+low;
     }
 
-    public static double dcdw(){
-        return dcda() * dadz() * dzdw();
+    public static double Dcdw(double a,double y, double z, ActivationFunction activationFunction, Perceptron prevLayer){
+        return dcda(a,y) * dadz(z,activationFunction) * dzdw(prevLayer);
+    }
+    public static double Dcda(double a,double y, double z, ActivationFunction activationFunction, Double weight){
+        return dcda(a,y) * dadz(z,activationFunction) * dzda(weight);
     }
     private static double dcda(double a, double y){
         return 2*(a-y);
     }
-    private static double dadz(double a, Layer prevLayer){
-
+    private static double dadz(double z,ActivationFunction activationFunction){
+        return activationFunction.getDerivative(z);
     }
 
-    private static double dzdw(Layer prevLayer){
-        double sum = 0.0;
-        for(Perceptron perceptron : prevLayer.getPerceptrons()){
-            sum += perceptron.getOutput();
-        }
-        return  sum;
+    private static double dzdw(Perceptron prevNode ){
+        return  prevNode.getOutput();
     }
+    private static double dzda(Double weight){
+        return weight;
+    }
+
 
 }
